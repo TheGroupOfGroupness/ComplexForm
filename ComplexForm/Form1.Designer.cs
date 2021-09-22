7cfe9f01dfeb0571f53ce57d13e8ab879ad5b272
Mckyle-Singh <80856342+Mckyle-Singh@users.noreply.github.com>
2021-09-21 17:08:03 -0700

    Add project files.

---
+++ ComplexForm/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace ComplexForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel_Menu = new System.Windows.Forms.Panel();
+            this.Ticket_Butt = new FontAwesome.Sharp.IconButton();
+            this.AddressButt = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Panel_TitleBar = new System.Windows.Forms.Panel();
+            this.lblTitleCurrentForm = new System.Windows.Forms.Label();
+            this.IconCurrentForm = new FontAwesome.Sharp.IconPictureBox();
+            this.PanelShadow = new System.Windows.Forms.Panel();
+            this.DesktopPanel = new System.Windows.Forms.Panel();
+            this.Panel_Menu.SuspendLayout();
+            this.Panel_TitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCurrentForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_Menu
+            // 
+            this.Panel_Menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.Panel_Menu.Controls.Add(this.Ticket_Butt);
+            this.Panel_Menu.Controls.Add(this.AddressButt);
+            this.Panel_Menu.Controls.Add(this.panel1);
+            this.Panel_Menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel_Menu.Location = new System.Drawing.Point(0, 0);
+            this.Panel_Menu.Name = "Panel_Menu";
+            this.Panel_Menu.Size = new System.Drawing.Size(241, 596);
+            this.Panel_Menu.TabIndex = 0;
+            // 
+            // Ticket_Butt
+            // 
+            this.Ticket_Butt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Ticket_Butt.FlatAppearance.BorderSize = 0;
+            this.Ticket_Butt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ticket_Butt.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Ticket_Butt.IconChar = FontAwesome.Sharp.IconChar.TicketAlt;
+            this.Ticket_Butt.IconColor = System.Drawing.Color.Gainsboro;
+            this.Ticket_Butt.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.Ticket_Butt.IconSize = 32;
+            this.Ticket_Butt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Ticket_Butt.Location = new System.Drawing.Point(0, 170);
+            this.Ticket_Butt.Name = "Ticket_Butt";
+            this.Ticket_Butt.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.Ticket_Butt.Size = new System.Drawing.Size(241, 60);
+            this.Ticket_Butt.TabIndex = 2;
+            this.Ticket_Butt.Text = "Log Tciket";
+            this.Ticket_Butt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Ticket_Butt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Ticket_Butt.UseVisualStyleBackColor = true;
+            this.Ticket_Butt.Click += new System.EventHandler(this.Ticket_Butt_Click);
+            // 
+            // AddressButt
+            // 
+            this.AddressButt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddressButt.FlatAppearance.BorderSize = 0;
+            this.AddressButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddressButt.ForeColor = System.Drawing.Color.Gainsboro;
+            this.AddressButt.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
+            this.AddressButt.IconColor = System.Drawing.Color.Gainsboro;
+            this.AddressButt.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AddressButt.IconSize = 32;
+            this.AddressButt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddressButt.Location = new System.Drawing.Point(0, 110);
+            this.AddressButt.Name = "AddressButt";
+            this.AddressButt.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.AddressButt.Size = new System.Drawing.Size(241, 60);
+            this.AddressButt.TabIndex = 1;
+            this.AddressButt.Text = "Owner Details";
+            this.AddressButt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddressButt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AddressButt.UseVisualStyleBackColor = true;
+            this.AddressButt.Click += new System.EventHandler(this.AddressButt_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // Panel_TitleBar
+            // 
+            this.Panel_TitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.Panel_TitleBar.Controls.Add(this.lblTitleCurrentForm);
+            this.Panel_TitleBar.Controls.Add(this.IconCurrentForm);
+            this.Panel_TitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_TitleBar.Location = new System.Drawing.Point(241, 0);
+            this.Panel_TitleBar.Name = "Panel_TitleBar";
+            this.Panel_TitleBar.Size = new System.Drawing.Size(907, 80);
+            this.Panel_TitleBar.TabIndex = 1;
+            this.Panel_TitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel_TitleBar_MouseDown);
+            // 
+            // lblTitleCurrentForm
+            // 
+            this.lblTitleCurrentForm.AutoSize = true;
+            this.lblTitleCurrentForm.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblTitleCurrentForm.Location = new System.Drawing.Point(138, 35);
+            this.lblTitleCurrentForm.Name = "lblTitleCurrentForm";
+            this.lblTitleCurrentForm.Size = new System.Drawing.Size(50, 20);
+            this.lblTitleCurrentForm.TabIndex = 1;
+            this.lblTitleCurrentForm.Text = "Home";
+            // 
+            // IconCurrentForm
+            // 
+            this.IconCurrentForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.IconCurrentForm.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.IconCurrentForm.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.IconCurrentForm.IconColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.IconCurrentForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.IconCurrentForm.IconSize = 40;
+            this.IconCurrentForm.Location = new System.Drawing.Point(28, 25);
+            this.IconCurrentForm.Name = "IconCurrentForm";
+            this.IconCurrentForm.Size = new System.Drawing.Size(40, 40);
+            this.IconCurrentForm.TabIndex = 0;
+            this.IconCurrentForm.TabStop = false;
+            // 
+            // PanelShadow
+            // 
+            this.PanelShadow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(24)))), ((int)(((byte)(58)))));
+            this.PanelShadow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelShadow.Location = new System.Drawing.Point(241, 80);
+            this.PanelShadow.Name = "PanelShadow";
+            this.PanelShadow.Size = new System.Drawing.Size(907, 9);
+            this.PanelShadow.TabIndex = 2;
+            // 
+            // DesktopPanel
+            // 
+            this.DesktopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.DesktopPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DesktopPanel.Location = new System.Drawing.Point(241, 89);
+            this.DesktopPanel.Name = "DesktopPanel";
+            this.DesktopPanel.Size = new System.Drawing.Size(907, 507);
+            this.DesktopPanel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 596);
+            this.Controls.Add(this.DesktopPanel);
+            this.Controls.Add(this.PanelShadow);
+            this.Controls.Add(this.Panel_TitleBar);
+            this.Controls.Add(this.Panel_Menu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Panel_Menu.ResumeLayout(false);
+            this.Panel_TitleBar.ResumeLayout(false);
+            this.Panel_TitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCurrentForm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel_Menu;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton Ticket_Butt;
+        private FontAwesome.Sharp.IconButton AddressButt;
+        private System.Windows.Forms.Panel Panel_TitleBar;
+        private System.Windows.Forms.Label lblTitleCurrentForm;
+        private FontAwesome.Sharp.IconPictureBox IconCurrentForm;
+        private System.Windows.Forms.Panel PanelShadow;
+        private System.Windows.Forms.Panel DesktopPanel;
+    }
+}
+
---
+++ ComplexForm/OwnerDetails.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace ComplexForm
+{
+    partial class OwnerDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnitID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMoveInDate = new System.Windows.Forms.DateTimePicker();
+            this.txtOwnerList = new System.Windows.Forms.ListBox();
+            this.Add_Butt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unit ID";
+            // 
+            // txtUnitID
+            // 
+            this.txtUnitID.Location = new System.Drawing.Point(160, 71);
+            this.txtUnitID.Name = "txtUnitID";
+            this.txtUnitID.Size = new System.Drawing.Size(125, 27);
+            this.txtUnitID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Move in Date";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(160, 123);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 27);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(160, 181);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(125, 27);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(160, 232);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(125, 27);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtMoveInDate
+            // 
+            this.txtMoveInDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtMoveInDate.Location = new System.Drawing.Point(160, 270);
+            this.txtMoveInDate.Name = "txtMoveInDate";
+            this.txtMoveInDate.Size = new System.Drawing.Size(125, 27);
+            this.txtMoveInDate.TabIndex = 9;
+            // 
+            // txtOwnerList
+            // 
+            this.txtOwnerList.FormattingEnabled = true;
+            this.txtOwnerList.ItemHeight = 20;
+            this.txtOwnerList.Location = new System.Drawing.Point(432, 66);
+            this.txtOwnerList.Name = "txtOwnerList";
+            this.txtOwnerList.Size = new System.Drawing.Size(303, 224);
+            this.txtOwnerList.TabIndex = 10;
+            // 
+            // Add_Butt
+            // 
+            this.Add_Butt.Location = new System.Drawing.Point(137, 348);
+            this.Add_Butt.Name = "Add_Butt";
+            this.Add_Butt.Size = new System.Drawing.Size(94, 29);
+            this.Add_Butt.TabIndex = 11;
+            this.Add_Butt.Text = "ADD";
+            this.Add_Butt.UseVisualStyleBackColor = true;
+            // 
+            // OwnerDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Add_Butt);
+            this.Controls.Add(this.txtOwnerList);
+            this.Controls.Add(this.txtMoveInDate);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUnitID);
+            this.Controls.Add(this.label1);
+            this.Name = "OwnerDetails";
+            this.Text = "OwnerDetails";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnitID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DateTimePicker txtMoveInDate;
+        private System.Windows.Forms.ListBox txtOwnerList;
+        private System.Windows.Forms.Button Add_Butt;
+    }
+}
